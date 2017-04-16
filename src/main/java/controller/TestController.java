17cf63d1ea152cfd9ad959e6074d584702abbd52
Mihail Kolomiets <mihail.kolomiets@gmail.com>
2017-04-16 17:37:33 +0300

    jstl and controller...

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by mihail on 16.04.17.
+ */
+@WebServlet (urlPatterns = "/test")
+public class TestController extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("message", "hello test");
+        req.getRequestDispatcher("/test.jsp").forward(req, resp);
+    }
+}
