08e527e07c97b884636f1d8dd1651de49e45c1ce
Mihail <mihail.kolomiets@gmail.com>
2017-12-03 20:04:55 +0200

    Update Variable.java

--- src/main/java/util/Variable.java
+++ src/main/java/util/Variable.java
@@ -4,5 +4,5 @@ package util;
  * Created by mihail on 09.10.17.
  */
 public class Variable {
-    public final static String LOCALHOST = "77.244.39.136";
+    public final static String LOCALHOST = "goo.gl/ZTcEdk";
 }
