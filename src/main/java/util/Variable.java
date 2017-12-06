ef2eed7be92e693c185e0fa76674f34e324e0375
Mihail <mihail.kolomiets@gmail.com>
2017-12-06 23:09:11 +0200

    Update Variable.java

--- src/main/java/util/Variable.java
+++ src/main/java/util/Variable.java
@@ -4,5 +4,5 @@ package util;
  * Created by mihail on 09.10.17.
  */
 public class Variable {
-    public final static String LOCALHOST = "goo.gl/ZTcEdk";
+    public final static String LOCALHOST = "openshift-quickstarts-any.193b.starter-ca-central-1.openshiftapps.com";
 }
